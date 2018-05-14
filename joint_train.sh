0b992fccc84e6d528919ee7c8c0980a134730b16
wangyirui <yiruiwang06@gmail.com>
2018-05-13 22:22:50 -0400

    small fix

--- joint_train.sh
+++ joint_train.sh
@@ -1,1 +1,2 @@
+cd ~/Adversarial-NMT
 CUDA_VISIBLE_DEVICES=`free-gpu -n 2` python ~/Adversarial-NMT/joint_train.py --data data-bin/wmt14.en-de/  --joint-batch-size 32 --gpuid 0 --clip-norm 1.0 --beam 1
